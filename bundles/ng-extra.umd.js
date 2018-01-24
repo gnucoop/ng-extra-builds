66b286f0eebfcf2e50db724283dfed3c9e56a1c5
trik <marco.marche@gnucoop.com>
2018-01-24 11:08:06 +0000

    629781c fix(chore): docs deploy ci script not working

--- bundles/ng-extra.umd.js
+++ bundles/ng-extra.umd.js
@@ -49,7 +49,7 @@
 /**
  * Current version of Ng-Extra.
  */
-var VERSION = new _angular_core.Version('0.1.0-0b58b8a');
+var VERSION = new _angular_core.Version('0.1.0-629781c');
 
 exports.VERSION = VERSION;
 exports.Geolocation = _gnucoop_ngExtra_geolocation.Geolocation;
--- esm2015/ng-extra.js
+++ esm2015/ng-extra.js
@@ -46,7 +46,7 @@ import { Geolocation, GeolocationModule } from '@gnucoop/ng-extra/geolocation';
 /**
  * Current version of Ng-Extra.
  */
-const VERSION = new Version('0.1.0-0b58b8a');
+const VERSION = new Version('0.1.0-629781c');
 
 /**
  * @fileoverview added by tsickle
